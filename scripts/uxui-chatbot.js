662ed2d3661c3d5400f23f5b52c36cdbfe1f05c1
Dinh Le <dinh.v.le@gmail.com>
2016-05-18 16:42:28 -0500

    integrate cleverbot

--- scripts/uxui-chatbot.js
+++ scripts/uxui-chatbot.js
@@ -1,7 +1,7 @@
 $(function() {
   // Declaring variables
   var $chatInput = $('#chat');
-  var isTyping;
+  // var isTyping = false;
 
   // Event listening on Chat box
   $('#chat').keypress(function parseText(event){
@@ -14,6 +14,9 @@ $(function() {
         $chatInput.val(""); //clear chat input
         createMessage("user", message);
         console.log(message);
+        setTimeout(function () {
+          botRespondTo(message);
+        }, 1000);
   		}
   	}
   });
@@ -39,18 +42,46 @@ $(function() {
     }
   }
 
+  // Function to get bot to reply to messages
+  // function botRespondTo(message) {
+  //   var response = "does not compute";
+  //   message = message.toLowerCase();
+  //
+  //   if(message.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
+	// 		response = "Hi, welcome to my page";
+  //
+	// 	if(message.match('what[^ ]* up') || message.match('sup') || message.match('how are you'))
+	// 		response = "I'm doing well, how are you?";
+  //
+  //   if(message.match('what[^ ]* your contact') || message.match('contact'))
+  //     response = "You can contact me at dinh.v.le@gmail.com";
+  //
+  //   if(message.match('how[^ ]* the weather') || message.match('weather'))
+  //     response = "I don't know, I've been stuck inside!";
+  //
+  //   if(message.match('what[^ ]* do for fun') || message.match('what[^ ]* your hobbies'))
+  //     response = "I like biking, eating and learning new things";
+  //
+  //   if(message.match('(bye|goodbye|cya|chao)(\\s|!|\\.|$)'))
+	// 		response = "See you later";
+  //
+  //   createMessage("bot", response);
+  //
+  // }
 
+  // Setting up cleverbot
+  function botRespondTo(message) {
+    var bot = new cleverbot("4Pxm8fgmOt9TIFOg", "B9snhX07iLDEBhvtEI3uYyxkjmy1jFav"); // Set up keys
+    bot.setNick("uxuibottest"); // Set a nickname
 
+    bot.create(function (err, uxuibottest) { // Initialize Cleverbot
 
+    	bot.ask(message, function (err, cleverbotResponse) {
+        console.log(cleverbotResponse);
+        createMessage("bot", cleverbotResponse);
+    	});
+    });
 
-
-
-
-
-
-
-
-
-
+  }
 
 });
